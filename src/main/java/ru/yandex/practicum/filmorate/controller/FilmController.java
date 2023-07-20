8172fb13caee0ffeafc8269bbae60e90e93c4afc
ilya-noize <shumakoff.ilya@yandex.ru>
2023-07-20 03:44:06 +0300

    Pull Request #2 add friends in users; add likes in films;

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -9,16 +9,6 @@ import ru.yandex.practicum.filmorate.service.FilmService;
 import javax.validation.Valid;
 import java.util.List;
 
-/**
- * POST /films — Добавляет фильма.
- * PUT /films — Обновляет фильма.
- * PUT /films/{id}/like/{userId} — Пользователь ставит лайк фильму.
- * DELETE /films/{id}/like/{userId} — Пользователь удаляет лайк.
- * GET /films — Возвращает список всех фильмов
- * GET /films/popular?count={count} — Возвращает список из первых count* фильмов по количеству лайков.
- * * Если значение параметра count не задано, вернуть первые 10.
- */
-
 @RestController
 @Slf4j
 @RequestMapping("/films")
@@ -30,12 +20,6 @@ public class FilmController extends Controller {
         this.filmService = filmService;
     }
 
-    /**
-     * Добавляет фильма.
-     *
-     * @param film фильм
-     * @return фильм
-     */
     @PostMapping
     public Film create(@Valid @RequestBody Film film) {
         log.info("Получен запрос POST к endpoint-у: /films");
@@ -44,75 +28,38 @@ public class FilmController extends Controller {
         return film;
     }
 
-    /**
-     * Обновляет фильма.
-     *
-     * @param film фильм
-     * @return фильм
-     */
     @PutMapping
     public Film update(@Valid @RequestBody Film film) {
         log.info("Получен запрос PUT к endpoint-у: /films");
         return filmService.update(film);
     }
 
-    /**
-     * Пользователь ставит лайк фильму.
-     *
-     * @param filmId уин фильма
-     * @param userId уин пользователя
-     */
     @PutMapping("/{filmId}/like/{userId}")
     public void addLike(@PathVariable String filmId, String userId) { // todo test
         log.info("Получен запрос PUT к endpoint-у: /films" + filmId + "/like/" + userId);
         filmService.addLike(filmId, userId);
     }
 
-    /**
-     * Пользователь удаляет лайк фильму.
-     *
-     * @param filmId уин фильма
-     * @param userId уин пользователя
-     */
     @DeleteMapping("/{filmId}/like/{userId}")
     public void deleteLike(@PathVariable String filmId, String userId) { // todo test
         log.info("Получен запрос PUT к endpoint-у: /films/" + filmId + "/like/" + userId);
         filmService.deleteLike(filmId, userId);
     }
 
-    /**
-     * Возвращает список всех фильмов
-     *
-     * @return список
-     */
     @GetMapping()
     public List<Film> getAll() {
         log.info("Получен запрос GET к endpoint-у: /films");
         return filmService.getAll();
     }
-
-    /**
-     * Возвращает фильм
-     *
-     * @return 1 pcs
-     */
     @GetMapping("/{id}")
     public Film get(@PathVariable String id) {
         log.info("Получен запрос GET к endpoint-у: /films");
         return filmService.get(id);
     }
 
-    /**
-     * Возвращает список из первых count* фильмов по количеству лайков.
-     *
-     * @param count def 10
-     * @return список
-     */
     @GetMapping("/films/popular?count={count}")
     public List<Film> getPopular(@RequestParam(value = "count", defaultValue = "10", required = false) String count) {
         log.info("Получен запрос GET к endpoint-у: /films/popular?count=" + count);
         return filmService.getPopular(count);
     }
-
-
 }
