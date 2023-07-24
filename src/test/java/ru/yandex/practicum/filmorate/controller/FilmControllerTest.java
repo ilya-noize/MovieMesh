e15def65fa3f22dc69505ecb81887b24ab2a9844
ilya-noize <shumakoff.ilya@yandex.ru>
2023-07-25 00:25:22 +0300

    Pull Request #2 abstract class MainStorage<T> for Films, Users; abstract class MainService<T> for Films, Users;
    APPLICATION FAILED TO START
    Description:
    Parameter 1 of constructor in ru.yandex.practicum.filmorate.service.FilmLikesService required a bean of type 'ru.yandex.practicum.filmorate.service.MainService' that could not be found.
    
    Action:
    Consider defining a bean of type 'ru.yandex.practicum.filmorate.service.MainService' in your configuration.
    
    fix checkstyle;

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -165,7 +165,7 @@ class FilmControllerTest {
     @Test
     void addLike() {
         try {
-            controller.addLike(null , null);
+            controller.addLike(null, null);
         } catch (Exception e) {
             assertNull(e.getMessage());
         }
