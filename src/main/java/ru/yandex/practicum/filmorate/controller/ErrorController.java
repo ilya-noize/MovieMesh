234b5ee7c6dc19628145cc544518f3e426bf7d69
ilya-noize <shumakoff.ilya@yandex.ru>
2023-07-29 03:11:23 +0300

    Pull Request #2 catch Throwable with HttpStatus.500

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorController.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,9 +14,9 @@ import ru.yandex.practicum.filmorate.model.ErrorResponse;
 @RestControllerAdvice
 @Slf4j
 public class ErrorController {
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+    @ExceptionHandler(Throwable.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleMethodArgumentNotValidException(Throwable e) {
         log.error("Method Argument Not Valid - Exception:{}", (Object[]) e.getStackTrace());
         return new ErrorResponse(e.getMessage());
     }
