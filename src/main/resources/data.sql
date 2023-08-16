696ce7c82eecb6329c04ada0cbffc46fb30fc27a
ilya-noize <shumakoff.ilya@yandex.ru>
2023-08-16 22:55:51 +0300

    prepare database to working

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,22 +6,22 @@
 --  rescue Princess Leia from the mysterious Darth Vader.",
 --  121, 25-05-1977, 1, 1);
 
-DELETE FROM films_likes;
-DELETE FROM friends;
-DELETE FROM users;
-DELETE FROM films;
+delete from films_like;
+delete from friends;
+delete from users;
+delete from films;
 
-ALTER TABLE users ALTER COLUMN id RESTART WITH 1;
-ALTER TABLE films ALTER COLUMN id RESTART WITH 1;
+alter table users alter COLUMN id RESTART with 1;
+alter table films alter COLUMN id RESTART with 1;
 
-MERGE INTO mpa_rating KEY (id)
+merge into mpa_rating KEY (id)
     VALUES (1, 'G', 'Нет возрастных ограничений'),
            (2, 'PG', 'Рекомендуется присутствие родителей'),
            (3, 'PG-13', 'Детям до 13 лет просмотр не желателен'),
            (4, 'R', 'Лицам до 17 лет обязательно присутствие взрослого'),
            (5, 'NC-17', 'Лицам до 18 лет просмотр запрещен');
 
-MERGE INTO genres KEY(id)
+merge into genres KEY(id)
     VALUES (1, 'Комедия'),
            (2, 'Драма'),
            (3, 'Мультфильм'),
