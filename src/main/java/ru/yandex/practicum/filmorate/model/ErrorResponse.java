7a2f75a963c934b3ff0d1b5005948cce53605237
ilya-noize <shumakoff.ilya@yandex.ru>
2023-08-16 23:36:02 +0300

    final class

--- src/main/java/ru/yandex/practicum/filmorate/model/ErrorResponse.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/ErrorResponse.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-public class ErrorResponse {
+public final class ErrorResponse {
     @Getter
     private final String error;
 }
