14b43b7f54a7dd304612ab2de77b5c6185bd5d7b
ilya-noize <shumakoff.ilya@yandex.ru>
2023-08-16 23:45:49 +0300

    refactoring. constructor [failed Postman test!]; rename fields; final class;

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -11,14 +11,12 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
-import java.util.HashSet;
 import java.util.Set;
-
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Film {
+public final class Film {
     private static final String RELEASE_DATE_LIMIT = "1895-12-28";
     @EqualsAndHashCode.Exclude
     @Positive(message = "The movie ID is a positive natural number.")
@@ -26,21 +24,36 @@ public class Film {
     @NotNull(message = "The name of the movie cannot be null.")
     @NotBlank(message = "The title of the movie cannot be blank.")
     private String name;
+    @CorrectReleaseDate(value = RELEASE_DATE_LIMIT, message = "Release date no earlier than 28 DEC 1895 and no later than today")
+    private LocalDate releaseDate;
     @Size(max = 200, message = "The description length is no more than 200 characters.")
     private String description;
     @Positive(message = "The duration of the movie is a positive natural number.")
     private Integer duration;
-    @CorrectReleaseDate(value = RELEASE_DATE_LIMIT, message = "Release date no earlier than 28 DEC 1895 and no later than today")
-    private LocalDate release;
-    @Positive
-    private Long mpaRatingId;
+    private Integer rate;
+    private MPARating mpa;
+    //    private Set<GenresFilm> genres;
+    private Set<Long> genres;
     private Set<Long> likes;
 
-    public Film(String name, String description, LocalDate release, int duration) {
+    public Film(
+            String name,
+            String description,
+            LocalDate releaseDate,
+            int duration,
+            Integer rate,
+            MPARating mpa,
+            Set<Long> genres,
+//            Set<GenresFilm> genres,
+            Set<Long> likes
+    ) {
         this.name = name;
         this.description = description;
-        this.release = release;
+        this.releaseDate = releaseDate;
         this.duration = duration;
-        this.likes = new HashSet<>();
+        this.rate = rate;
+        this.mpa = mpa;
+        this.genres = genres;
+        this.likes = likes;
     }
 }
