18b82e63ee7e58c12c5325d63986a621c58f8997
ilya-noize <shumakoff.ilya@yandex.ru>
2023-08-17 23:26:23 +0300

    switch on MasterStorageDAO;

--- src/main/java/ru/yandex/practicum/filmorate/service/GenreService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/GenreService.java
@@ -3,14 +3,14 @@ package ru.yandex.practicum.filmorate.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.dao.MasterStorageDAO;
 import ru.yandex.practicum.filmorate.model.Genre;
-import ru.yandex.practicum.filmorate.storage.MasterStorage;
 
 @Slf4j
 @Service
 public class GenreService extends MasterService<Genre> {
     @Autowired
-    public GenreService(MasterStorage<Genre> storage) {
+    public GenreService(MasterStorageDAO<Genre> storage) {
         super(storage);
     }
 }
