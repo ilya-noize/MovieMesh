9203ed886f813173952e4154da55a0e2444be7df
ilya-noize <shumakoff.ilya@yandex.ru>
2023-08-25 04:17:50 +0300

    refactoring model User

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -7,7 +7,6 @@ import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.*;
 import java.time.LocalDate;
-import java.util.Set;
 
 @Data
 @NoArgsConstructor
@@ -16,25 +15,22 @@ public final class User {
     @EqualsAndHashCode.Exclude
     @Positive(message = "The user ID is a positive natural number.")
     private Long id;
-
     @Email(message = "The entered value is not an email address.")
     private String email;
-
     @Pattern(regexp = "^\\S*$", message = "The login cannot contain spaces.")
     @Size(min = 3, max = 20, message = "The login must be from 3 to 20 characters.")
     private String login;
     private String name;
-
     @Past(message = "The date of birth should only be in the past.")
     private LocalDate birthday;
-    private Set<Long> friends;
-    private Set<Long> likes;
 
-    public User(String login, String name, String email, LocalDate birthday, Set<Long> friends) {
+    public User(String login,
+                String name,
+                String email,
+                LocalDate birthday) {
         this.login = login;
         this.name = name;
         this.email = email;
         this.birthday = birthday;
-        this.friends = friends;
     }
 }
