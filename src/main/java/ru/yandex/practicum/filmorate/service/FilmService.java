04c87e6d3f425f3c9af813579f5f474027d7d177
ilya-noize <shumakoff.ilya@yandex.ru>
2023-08-30 01:42:44 +0300

    refactor:  removed the abstraction structure;  moving some methods to the DAO layer;  clean code;

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -1,156 +1,90 @@
 package ru.yandex.practicum.filmorate.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.filmorate.constraint.CorrectReleaseDate;
-import ru.yandex.practicum.filmorate.dao.MasterStorageDAO;
-import ru.yandex.practicum.filmorate.dao.rowMapper.GenreRowMapper;
+import ru.yandex.practicum.filmorate.dao.FilmDAO;
+import ru.yandex.practicum.filmorate.dao.FilmGenresDAO;
+import ru.yandex.practicum.filmorate.dao.GenreDAO;
+import ru.yandex.practicum.filmorate.dao.MPARatingDAO;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
-import ru.yandex.practicum.filmorate.model.GenresFilm;
-import ru.yandex.practicum.filmorate.model.MPARating;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
-import javax.validation.constraints.Size;
-import java.time.LocalDate;
-import java.util.Comparator;
+import java.util.ArrayList;
 import java.util.List;
-
-import static java.util.stream.Collectors.toList;
-import static ru.yandex.practicum.filmorate.model.Film.RELEASE_DATE_LIMIT;
+import java.util.Map;
+import java.util.Set;
 
 @Slf4j
 @Service
-public class FilmService extends MasterService<Film> {
+@RequiredArgsConstructor
+public class FilmService {
     private static Long filmId;
-    private final MasterStorageDAO<GenresFilm> genresFilmStorage;
-    private final MasterStorageDAO<Genre> genreStorage;
-    private final MasterStorageDAO<MPARating> mpaRatingStorage;
-    private final Comparator<Film> sortLikes = Comparator.comparing(film -> film.getLikes().size());
+    private final FilmDAO filmDAO;
+    private final FilmGenresDAO filmGenresDAO;
+    private final GenreDAO genreDAO;
+    private final MPARatingDAO mpaRatingDAO;
 
-    public FilmService(MasterStorageDAO<Film> storage,
-                       MasterStorageDAO<GenresFilm> genresFilmStorage,
-                       MasterStorageDAO<Genre> genreStorage,
-                       MasterStorageDAO<MPARating> mpaRatingStorage) {
-        super(storage);
-        this.genresFilmStorage = genresFilmStorage;
-        this.genreStorage = genreStorage;
-        this.mpaRatingStorage = mpaRatingStorage;
+    public Film create(Film film) {
+        log.info("[+][S] Film: \n film:{}", film);
+        filmDAO.create(film);
+        filmId = film.getId();
+        List<Genre> genres = film.getGenres();
+        if (genres != null) {
+            filmGenresDAO.add(filmId, genres);
+        }
+        return get(filmId);
+    }
+
+    public Film update(Film film) {
+        filmId = film.getId();
+        List<Genre> genres = film.getGenres();
+        log.info("[u][S] Film: \n film:{}\n genres:{}", film, genres);
+        isExist(filmId);
+        filmDAO.update(film);
+        if (genres != null) {
+            log.info("[i] service: update genres:{}", genres);
+            filmGenresDAO.delete(filmId);
+            filmGenresDAO.add(filmId, genres);
+        }
+        return get(filmId);
+    }
+
+    public Film get(Long id) {
+        filmId = id;
+        Film film = filmDAO.get(filmId);
+        log.info("[>][S] Film\nFilm = {}", film);
+        film.setMpa(mpaRatingDAO.get(film.getMpa().getId()));
+
+        Map<Long, List<Genre>> genres = genreDAO.getFilmGenres(Set.of(filmId));
+        if (genres.isEmpty()) {
+            film.setGenres(new ArrayList<>());
+        } else {
+            film.setGenres(genres.get(filmId));
+        }
+        return film;
+    }
+
+    public List<Film> getAll() {
+        List<Film> films = filmDAO.getAll();
+        log.info("[>][S] All Films (size = {})", films.size());
+        return films;
     }
 
     /**
      * Возвращает список популярных фильмов у пользователей
      *
-     * @param supposedCount количество фильмов
+     * @param count количество фильмов
      * @return список
      */
-    public List<Film> getPopular(Long supposedCount) {
-        log.info("Returning the TOP{} popular movies from users", supposedCount);
-        return this.getAll().stream()
-                .sorted(sortLikes.reversed())
-                .limit(supposedCount)
-                .collect(toList());
+    public List<Film> getPopular(Long count) {
+        log.info("Returning the TOP-{} popular movies from users", count);
+        return filmDAO.getPopular(count);
     }
 
-    @Override
-    public Film create(Film film) {
-        film = super.create(valid(film));
-        filmId = film.getId();
-        List<Genre> genres = film.getGenres();
-        if (genres != null) {
-            genres.forEach(genre ->
-                    genresFilmStorage.create(
-                            new GenresFilm(filmId, genre.getId())
-                    ));
+    private void isExist(Long... ids) {
+        for (Long id : ids) {
+            get(id);
         }
-        return film;
-    }
-
-    @Override
-    public Film update(Film film) {
-        filmId = film.getId();
-        List<Genre> genresId = film.getGenres();
-        log.info("[u] Film Service: \n film:{}\n genresId:{}", film, genresId);
-        super.isExist(filmId);
-        super.update(film);
-        if (genresId != null) {
-            log.info("[i] service: update genresId:{}", genresId);
-            genresFilmStorage.delete(filmId, null);
-            genresId.forEach(genreId -> {
-                GenresFilm genresFilm = new GenresFilm(filmId, genreId.getId());//genreId);
-                genresFilmStorage.create(genresFilm);
-            });
-        }
-        return get(filmId);
-    }
-
-    @Override
-    public Film get(Long id) {
-        Film film = super.get(id);
-        log.info("[>] FilmService\ngetFilm(id = {})\nFilm = {}", id, film);
-        filmId = film != null ? film.getId() : null;
-        if (filmId != null) {
-            List<Long> likes = getUserLikes();
-
-            film.setMpa(mpaRatingStorage.get(film.getMpa().getId()));
-            film.setRate(likes.size());
-            film.setGenres(getGenresByFilm());
-            film.setLikes(likes);
-        }
-        return film;
-    }
-
-    @Override
-    public List<Film> getAll() {
-        return super.getAll().stream()
-                .map(Film::getId)
-                .map(this::get)
-                .collect(toList());
-    }
-
-    private List<Genre> getGenresByFilm() {
-        String sql = "SELECT G.ID, G.NAME FROM GENRES_FILM GF"
-                + " RIGHT JOIN FILMS F ON F.id = GF.FILM_ID"
-                + " RIGHT JOIN GENRES G ON G.id = GF.GENRE_ID"
-                + " WHERE F.ID = ? ORDER BY G.ID, G.NAME;";
-        return genresFilmStorage.getJdbcTemplate().query(sql, new GenreRowMapper(), filmId);
-    }
-
-    private List<Long> getUserLikes() {
-        String sql = "SELECT user_id FROM films_like WHERE film_id = ? ORDER BY user_id";
-        return genresFilmStorage.getJdbcTemplate().query(sql,
-                (rs, rowNum) -> rs.getLong("user_id"),
-                filmId);
-    }
-
-    private Film valid(Film film) {
-        film.setName(checkName(film));
-        film.setReleaseDate(checkRelease(film));
-        film.setDescription(checkDecriprtion(film));
-        film.setDuration(checkDuration(film));
-        return film;
-    }
-
-    @NotNull(message = "The name of the movie cannot be null.")
-    @NotBlank(message = "The title of the movie cannot be blank.")
-    private String checkName(Film film) {
-        return film.getName();
-    }
-
-    @CorrectReleaseDate(value = RELEASE_DATE_LIMIT, message = "The release date is not earlier than a certain date and not later than today")
-    private LocalDate checkRelease(Film film) {
-        return film.getReleaseDate();
-    }
-
-    @Size(max = 200, message = "The description length is no more than 200 characters.")
-    private String checkDecriprtion(Film film) {
-        return film.getDescription();
-    }
-
-    @Positive(message = "The duration of the movie is a positive natural number.")
-    private Integer checkDuration(Film film) {
-        return film.getDuration();
     }
 }
