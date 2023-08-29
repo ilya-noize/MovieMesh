db864f0bc08d07a9b1f8020b5606e6b7b6ee5705
ilya-noize <shumakoff.ilya@yandex.ru>
2023-08-30 01:43:06 +0300

    refactor:  clean code;

--- src/main/java/ru/yandex/practicum/filmorate/exception/FriendsException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/FriendsException.java
@@ -1,10 +1,6 @@
 package ru.yandex.practicum.filmorate.exception;
 
 public class FriendsException extends RuntimeException {
-    public FriendsException(String message) {
-        super(message);
-    }
-
     public FriendsException() {
         super("Which is much more valuable than your friendship with yourself.");
     }
