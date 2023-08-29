005a23c69be36a0037a9d78c70183ffdec865183
ilya-noize <shumakoff.ilya@yandex.ru>
2023-08-30 01:26:14 +0300

    refactor:  removed the abstraction structure;  made query optimization;

--- src/main/java/ru/yandex/practicum/filmorate/dao/FilmDAO.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/FilmDAO.java
@@ -1,7 +1,7 @@
 package ru.yandex.practicum.filmorate.dao;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
@@ -11,23 +11,25 @@ import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.dao.rowMapper.FilmRowMapper;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.Genre;
 
 import java.sql.Date;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.List;
+import java.util.*;
+
+import static java.util.stream.Collectors.toSet;
 
 @Slf4j
 @Component
 @Primary
-public final class FilmDAO extends MasterStorageDAO<Film> {
-    @Autowired
-    public FilmDAO(JdbcTemplate jdbcTemplate) {
-        super(jdbcTemplate);
-    }
+@RequiredArgsConstructor
+public final class FilmDAO {
+    private final JdbcTemplate jdbcTemplate;
+    private final GenreDAO genreDAO;
+    private final FilmGenresDAO filmGenresDAO;
+    private final MPARatingDAO mpaRatingDAO;
+    private final FilmRowMapper filmRowMapper;
 
-    @Override
     public Film create(Film film) {
         String sql = "INSERT INTO films (name, description, duration, releaseDate, mpa_rating_id)"
                 + " VALUES (?, ?, ?, ?, ?);";
@@ -41,18 +43,21 @@ public final class FilmDAO extends MasterStorageDAO<Film> {
             ps.setLong(5, film.getMpa().getId());
             return ps;
         };
-        getJdbcTemplate().update(psc, keyHolder);
-        Long filmId = keyHolder.getKey().longValue();
+        jdbcTemplate.update(psc, keyHolder);
+        Long filmId = Objects.requireNonNull(keyHolder.getKey()).longValue();
         film.setId(filmId);
+        List<Genre> filmGenres = film.getGenres();
+        if (filmGenres != null) {
+            filmGenresDAO.add(filmId, filmGenres);
+        }
         return film;
     }
 
-    @Override
     public Film update(Film film) {
-        String sql = "UPDATE films SET"
-                + " name = ?, description = ?, duration = ?, releaseDate = ?, mpa_rating_id=?"
-                + " WHERE id = ?;";
-        getJdbcTemplate().update(
+        String sql = "UPDATE FILMS SET"
+                + " NAME = ?, DESCRIPTION = ?, DURATION = ?, RELEASEDATE = ?, MPA_RATING_ID=?"
+                + " WHERE ID = ?;";
+        jdbcTemplate.update(
                 sql,
                 film.getName(),
                 film.getDescription(),
@@ -64,31 +69,47 @@ public final class FilmDAO extends MasterStorageDAO<Film> {
         return film;
     }
 
-    @Override
     public Film get(Long id) {
         String error = String.format("Film not found - id:%d not exist", id);
         String sql = "SELECT F.* FROM films F WHERE F.id = ? ORDER BY F.id";
-        return getJdbcTemplate().query(sql, this::make, id)
+        return jdbcTemplate.query(sql, filmRowMapper, id)
                 .stream().findFirst()
                 .orElseThrow(new NotFoundException(error));
     }
 
-    @Override
-    public void delete(Long... id) {
-        Long filmId = id[0];
-        String sql = "DELETE FROM films F WHERE id = ?;";
-        getJdbcTemplate().update(sql, filmId);
-    }
-
-    @Override
     public List<Film> getAll() {
         String sql = "SELECT F.* FROM films F"
                 + " ORDER BY F.ID;";
-        return getJdbcTemplate().query(sql, this::make);
+        List<Film> films = jdbcTemplate.query(sql, filmRowMapper);
+        if (films.isEmpty()) {
+            return new ArrayList<>();
+        }
+        return getAllDataFilm(films);
     }
 
-    @Override
-    public Film make(ResultSet rs, int rowNum) throws SQLException {
-        return new FilmRowMapper().mapRow(rs, rowNum);
+    private List<Film> getAllDataFilm(List<Film> films) {
+        Set<Long> filmIds = films.stream().map(Film::getId).collect(toSet());
+        Map<Long, List<Genre>> filmGenres = genreDAO.getFilmGenres(filmIds);
+        films.forEach(film -> {
+            if (filmGenres.isEmpty()) {
+                film.setGenres(new ArrayList<>());
+            } else {
+                film.setGenres(filmGenres.get(film.getId()));
+            }
+            film.setMpa(mpaRatingDAO.get(film.getMpa().getId()));
+        });
+        return films;
+    }
+
+    public List<Film> getPopular(Long count) {
+        String sql = "SELECT F.*, COUNT(FL.USER_ID) LIKES FROM FILMS_LIKE FL" +
+                " RIGHT JOIN FILMS F ON FL.FILM_ID = F.ID" +
+                " GROUP BY F.ID " +
+                " ORDER BY LIKES DESC LIMIT ?;";
+        List<Film> films = jdbcTemplate.query(sql, filmRowMapper, count);
+        if (films.isEmpty()) {
+            return new ArrayList<>();
+        }
+        return getAllDataFilm(films);
     }
 }
