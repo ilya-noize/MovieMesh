c83861ccfe45ad6b0e1e61b76a431caa62ad4343
ilya-noize <shumakoff.ilya@yandex.ru>
2023-08-30 01:56:06 +0300

    refactor:  @Component

--- src/main/java/ru/yandex/practicum/filmorate/dao/rowMapper/MPARatingRowMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/rowMapper/MPARatingRowMapper.java
@@ -1,11 +1,13 @@
 package ru.yandex.practicum.filmorate.dao.rowMapper;
 
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.MPARating;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+@Component
 public class MPARatingRowMapper implements RowMapper<MPARating> {
     @Override
     public MPARating mapRow(ResultSet rs, int rowNum) throws SQLException {
