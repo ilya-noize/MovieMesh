a3710c6fd66398e21213850ff25147aadf532278
ilya-noize <shumakoff.ilya@yandex.ru>
2023-08-30 01:11:06 +0300

    refactor: delete comments, add 9 genres, UpperCase;

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,31 +1,32 @@
---INSERT INTO films (name, description, duration, release, genre_id, mpa_rating_id)
---VALUES ('Star Wars: Episode IV – A New Hope',
--- "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot,
---  a Wookiee and two droids to save the galaxy from the Empire's
---  world-destroying battle station, while also attempting to
---  rescue Princess Leia from the mysterious Darth Vader.",
---  121, 25-05-1977, 1, 1);
+DELETE FROM FILMS_LIKE;
+DELETE FROM FILM_GENRES;
+DELETE FROM FRIENDS;
+DELETE FROM USERS;
+DELETE FROM FILMS;
 
-delete from films_like;
-delete from genres_film;
-delete from friends;
-delete from users;
-delete from films;
+ALTER TABLE USERS ALTER COLUMN ID RESTART WITH 1;
+ALTER TABLE FILMS ALTER COLUMN ID RESTART WITH 1;
 
-alter table users alter COLUMN id RESTART with 1;
-alter table films alter COLUMN id RESTART with 1;
-
-merge into mpa_rating KEY (id)
+MERGE INTO MPA_RATING KEY (ID)
     VALUES (1, 'G', 'Нет возрастных ограничений'),
            (2, 'PG', 'Рекомендуется присутствие родителей'),
            (3, 'PG-13', 'Детям до 13 лет просмотр не желателен'),
            (4, 'R', 'Лицам до 17 лет обязательно присутствие взрослого'),
            (5, 'NC-17', 'Лицам до 18 лет просмотр запрещен');
 
-merge into genres KEY(id)
-    VALUES (1, 'Комедия'),
-           (2, 'Драма'),
-           (3, 'Мультфильм'),
-           (4, 'Триллер'),
-           (5, 'Документальный'),
-           (6, 'Боевик');
+MERGE INTO GENRES KEY(ID)
+    VALUES (1,  'Комедия'),
+           (2,  'Драма'),
+           (3,  'Мультфильм'),
+           (4,  'Триллер'),
+           (5,  'Документальный'),
+           (6,  'Боевик'),
+           (7,  'Фантастика'),
+           (8,  'Вестерн'),
+           (9,  'Детектив'),
+           (10, 'Нуар'),
+           (11, 'Ужасы'),
+           (12, 'Политика'),
+           (13, 'Мюзикл'),
+           (14, 'Мелодрама'),
+           (15, 'Сказка');
