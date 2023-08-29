3a29662bec1aae34170855c06a6934ce279c0980
ilya-noize <shumakoff.ilya@yandex.ru>
2023-08-30 01:56:43 +0300

    refactor:  removed the abstraction structure;  methods have been created for working with the `mpa_rating` table;  clean code;

--- src/main/java/ru/yandex/practicum/filmorate/dao/MPARatingDAO.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/MPARatingDAO.java
@@ -1,84 +1,34 @@
 package ru.yandex.practicum.filmorate.dao;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.dao.rowMapper.MPARatingRowMapper;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.MPARating;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 @Slf4j
 @Component
 @Primary
-public final class MPARatingDAO extends MasterStorageDAO<MPARating> {
-    @Autowired
-    public MPARatingDAO(JdbcTemplate jdbcTemplate) {
-        super(jdbcTemplate);
-    }
+@RequiredArgsConstructor
+public final class MPARatingDAO {
+    private final JdbcTemplate jdbcTemplate;
+    private final MPARatingRowMapper mpaRatingRowMapper;
 
-    @Override
-    public MPARating create(MPARating mpaRating) {
-        String sql = "INSERT INTO mpa_rating (rating, description)"
-                + " VALUES (?, ?)";
-        KeyHolder keyHolder = new GeneratedKeyHolder();
-        PreparedStatementCreator psc = con -> {
-            PreparedStatement ps = con.prepareStatement(sql, new String[]{"id"});
-            ps.setString(1, mpaRating.getName());
-            ps.setString(2, mpaRating.getDescription());
-            return ps;
-        };
-        getJdbcTemplate().update(psc, keyHolder);
-        Long mpaId = keyHolder.getKey().longValue();
-        mpaRating.setId(mpaId);
-        return mpaRating;
-    }
-
-    @Override
-    public MPARating update(MPARating mpaRating) {
-        String sql = "UPDATE mpa_rating"
-                + " SET rating = ?, description = ? WHERE id = ?";
-        getJdbcTemplate().update(
-                sql,
-                mpaRating.getName(),
-                mpaRating.getDescription(),
-                mpaRating.getId());
-        return mpaRating;
-    }
-
-    @Override
     public MPARating get(Long id) {
         String sql = "SELECT * FROM mpa_rating WHERE id = ?";
         String error = String.format("MPARating not found - id:%d not exist", id);
-        return getJdbcTemplate().query(sql, this::make, id)
+        return jdbcTemplate.query(sql, mpaRatingRowMapper, id)
                 .stream().findFirst()
                 .orElseThrow(new NotFoundException(error));
     }
 
-    @Override
-    public void delete(Long... id) {
-        String sql = "DELETE FROM  WHERE id = ?;";
-        getJdbcTemplate().update(sql, id[0]);
-    }
-
-    @Override
     public List<MPARating> getAll() {
         String sql = "SELECT * FROM mpa_rating";
-        return getJdbcTemplate().query(sql, this::make);
-    }
-
-    @Override
-    public MPARating make(ResultSet rs, int rowNum) throws SQLException {
-        return new MPARating(rs.getLong("id"),
-                rs.getString("name"),
-                rs.getString("description"));
+        return jdbcTemplate.query(sql, mpaRatingRowMapper);
     }
 }
