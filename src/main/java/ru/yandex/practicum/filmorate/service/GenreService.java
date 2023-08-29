0a59f080c6452c139b42b21e28f087053f233532
ilya-noize <shumakoff.ilya@yandex.ru>
2023-08-30 02:00:03 +0300

    refactor:  removed the abstraction structure;  methods have been created for working with the `GENRES` table;

--- src/main/java/ru/yandex/practicum/filmorate/service/GenreService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/GenreService.java
@@ -1,16 +1,31 @@
 package ru.yandex.practicum.filmorate.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.filmorate.dao.MasterStorageDAO;
+import ru.yandex.practicum.filmorate.dao.GenreDAO;
 import ru.yandex.practicum.filmorate.model.Genre;
+
+import java.util.List;
 
 @Slf4j
 @Service
-public class GenreService extends MasterService<Genre> {
-    @Autowired
-    public GenreService(MasterStorageDAO<Genre> storage) {
-        super(storage);
+@RequiredArgsConstructor
+public class GenreService {
+    private final GenreDAO genreDAO;
+
+    public Genre get(Long id) {
+        isExist(id);
+        return genreDAO.get(id);
+    }
+
+    public List<Genre> getAll() {
+        return genreDAO.getAll();
+    }
+
+    private void isExist(Long... ids) {
+        for (Long id : ids) {
+            get(id);
+        }
     }
 }
