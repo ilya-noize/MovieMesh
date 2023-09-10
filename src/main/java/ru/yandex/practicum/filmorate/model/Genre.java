3ccbaeafb6ddb8eeaf32bd20e63c3362dd2cbc25
ilya-noize <shumakoff.ilya@yandex.ru>
2023-09-10 22:41:32 +0300

    refactor: final fields

--- src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
@@ -1,18 +1,14 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Positive;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public final class Genre {
     @Positive
-    private Long id;
+    private final Long id;
     @NotBlank
-    private String name;
+    private final String name;
 }
