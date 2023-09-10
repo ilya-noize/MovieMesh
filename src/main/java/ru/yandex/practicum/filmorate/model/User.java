ade86922947db1d01d7a980b17b3e4fdd64f9a86
ilya-noize <shumakoff.ilya@yandex.ru>
2023-09-10 22:47:53 +0300

    refactor: final fields; delete custom constructor;

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,36 +1,22 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.*;
 import java.time.LocalDate;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public final class User {
     @EqualsAndHashCode.Exclude
     @Positive(message = "The user ID is a positive natural number.")
-    private Long id;
+    private final Long id;
     @Email(message = "The entered value is not an email address.")
-    private String email;
+    private final String email;
     @Pattern(regexp = "^\\S*$", message = "The login cannot contain spaces.")
     @Size(min = 3, max = 20, message = "The login must be from 3 to 20 characters.")
-    private String login;
-    private String name;
+    private final String login;
+    private final String name;
     @Past(message = "The date of birth should only be in the past.")
-    private LocalDate birthday;
-
-    public User(String login,
-                String name,
-                String email,
-                LocalDate birthday) {
-        this.login = login;
-        this.name = name;
-        this.email = email;
-        this.birthday = birthday;
-    }
+    private final LocalDate birthday;
 }
