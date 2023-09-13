5806faed6ff5ec9b4ce183f389478e808c2c6323
ilya-noize <shumakoff.ilya@yandex.ru>
2023-09-13 22:24:21 +0300

    refactor: Exceptions return ResponseEntity;
    Signed-off-by: ilya-noize <shumakoff.ilya@yandex.ru>

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorController.java
@@ -2,10 +2,7 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.TypeMismatchException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,75 +10,74 @@ import org.springframework.web.method.annotation.MethodArgumentTypeMismatchExcep
 import ru.yandex.practicum.filmorate.exception.FriendsException;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.UserAlreadyExistException;
-import ru.yandex.practicum.filmorate.exception.ValidException;
 
-import java.time.LocalDate;
+import javax.validation.ValidationException;
+import java.time.ZonedDateTime;
 import java.util.Map;
 
-import static org.springframework.http.HttpStatus.*;
+import static java.time.Clock.system;
+import static java.time.Clock.systemDefaultZone;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestControllerAdvice
 @Slf4j
-public final class ErrorController extends Throwable {
+public final class ErrorController extends RuntimeException {
+    private static final ZonedDateTime NOW = ZonedDateTime.now(system(systemDefaultZone().getZone()));
 
     @ExceptionHandler({MethodArgumentTypeMismatchException.class, TypeMismatchException.class})
-    public ResponseEntity<Map<String, Object>> handleException(TypeMismatchException e) {
+    @ResponseStatus(BAD_REQUEST)
+    public ResponseEntity<?> handleException(Exception e) {
 
-        log.error("[!] Type Mismatch \n Exception:{}", e.getMessage());
-        return new ResponseEntity<>(makeMap(e, BAD_REQUEST), BAD_REQUEST);
-    }
-
-    @ExceptionHandler({BindException.class, MethodArgumentNotValidException.class})
-    public ResponseEntity<Map<String, Object>> handleException(BindException e) {
-
-        log.error("[!] Bind Exception \n Exception:{}", e.getMessage());
-        return new ResponseEntity<>(makeMap(e, BAD_REQUEST), BAD_REQUEST);
-    }
-
-    @ExceptionHandler(Throwable.class)
-    @ResponseStatus(INTERNAL_SERVER_ERROR)
-    public ResponseEntity<Map<String, Object>> handleMethodArgumentNotValidException(Throwable e) {
-
-        log.error("[!] Method Argument Not Valid \n Exception:{}", e.getMessage());
-        return new ResponseEntity<>(makeMap(e, INTERNAL_SERVER_ERROR), INTERNAL_SERVER_ERROR);
+        log.error("[!] Exception:\n {}", e.getMessage());
+        return ResponseEntity
+                .badRequest()
+                .lastModified(NOW)
+                .header("error", e.getLocalizedMessage())
+                .build();
     }
 
     @ExceptionHandler
     @ResponseStatus(NOT_FOUND)
-    public ResponseEntity<Map<String, Object>> handleUserAlreadyExistException(UserAlreadyExistException e) {
+    public ResponseEntity<?> handleUserAlreadyExistException(UserAlreadyExistException e) {
 
         log.error("[!] User Already Exist \n Exception:{}", (Object[]) e.getStackTrace());
-        return new ResponseEntity<>(makeMap(e, NOT_FOUND), NOT_FOUND);
+        return ResponseEntity
+                .badRequest()
+                .lastModified(NOW)
+                .header("error", e.getLocalizedMessage())
+                .build();
     }
 
-    @ExceptionHandler
+    @ExceptionHandler(ValidationException.class)
     @ResponseStatus(BAD_REQUEST)
-    public ResponseEntity<Map<String, Object>> handleValidException(ValidException e) {
+    public ResponseEntity<?> handleValidException(ValidationException e) {
 
         log.error("[!] Valid Exception \n Exception:{}", (Object[]) e.getStackTrace());
-        return new ResponseEntity<>(makeMap(e, BAD_REQUEST), BAD_REQUEST);
+        return ResponseEntity
+                .badRequest()
+                .lastModified(NOW)
+                .header("error", e.getLocalizedMessage())
+                .build();
     }
 
-    @ExceptionHandler
+    @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(NOT_FOUND)
     public ResponseEntity<Map<String, Object>> handleNotFoundException(NotFoundException e) {
-
-        log.error("[!] Not Found \n Exception:{}", (Object[]) e.getStackTrace());
-        return new ResponseEntity<>(makeMap(e, NOT_FOUND), NOT_FOUND);
+        return new ResponseEntity<>(Map.of(
+                "timestamp", NOW,
+                "error", e.getMessage()), NOT_FOUND);
     }
 
-    @ExceptionHandler
+    @ExceptionHandler(FriendsException.class)
     @ResponseStatus(BAD_REQUEST)
-    public ResponseEntity<Map<String, Object>> handleFriendsException(FriendsException e) {
+    public ResponseEntity<?> handleFriendsException(FriendsException e) {
 
         log.error("[!] Friends Exception \n Exception:{}", (Object[]) e.getStackTrace());
-        return new ResponseEntity<>(makeMap(e, BAD_REQUEST), BAD_REQUEST);
-    }
-
-    private Map<String, Object> makeMap(Throwable e, HttpStatus httpStatus) {
-
-        return Map.of("timestamp", LocalDate.now().toString(),
-                "message", e.getMessage(),
-                "status", httpStatus);
+        return ResponseEntity
+                .badRequest()
+                .lastModified(NOW)
+                .header("error", e.getLocalizedMessage())
+                .build();
     }
 }
